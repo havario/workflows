38c8d9f60ac871be34ca44dd413ce29ac487e8ab
honeok <yihaohey@gmail.com>
2025-04-11 23:08:09 +0800

    update

--- fun/x-ui/docker-entrypoint.sh
+++ fun/x-ui/docker-entrypoint.sh
@@ -4,9 +4,6 @@
 #
 # Copyright (c) 2025 honeok <honeok@duck.com>
 #
-# References:
-# https://github.com/233boy/sing-box
-#
 # Licensed under the GNU General Public License, version 2 only.
 # This program is distributed WITHOUT ANY WARRANTY.
 # See <https://www.gnu.org/licenses/old-licenses/gpl-2.0.html>.
