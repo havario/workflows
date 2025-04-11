e9eac3d105ebe69c967c577cc705ac21cd3cc82b
honeok <yihaohey@gmail.com>
2025-04-11 18:41:20 +0800

    update

--- fun/x-ui/docker-entrypoint.sh
+++ fun/x-ui/docker-entrypoint.sh
@@ -20,49 +20,49 @@
 #     v2-ui          migrate form v2-ui
 #     setting        set settings
 
-workdir="/usr/local/bin"
-usernameTemp=$(head -c 6 /dev/urandom | base64)
-passwdTemp=$(head -c 6 /dev/urandom | base64)
+WORKDIR="/usr/local/bin"
+USERNAMETEMP=$(head -c 6 /dev/urandom | base64)
+PASSWDTEMP=$(head -c 6 /dev/urandom | base64)
 
 # https://www.graalvm.org/latest/reference-manual/ruby/UTF8Locale
 export LANG=en_US.UTF-8
 
 gen_port() {
-    is_count=0
-    is_used_port=''
+    IS_COUNT=0
+    IS_USED_PORT=''
 
     is_test() {
-        [ ! "$is_used_port" ] && is_used_port=$(netstat -tunlp | sed -n 's/.*:\([0-9]\+\).*/\1/p' | sort -nu)
-        echo "$is_used_port" | sed 's/ /\n/g' | grep ^"${1}"$
+        [ ! "$IS_USED_PORT" ] && IS_USED_PORT=$(netstat -tunlp | sed -n 's/.*:\([0-9]\+\).*/\1/p' | sort -nu)
+        echo "$IS_USED_PORT" | sed 's/ /\n/g' | grep ^"${1}"$
         return
     }
 
     while :; do
-        ((is_count++))
-        if [ "$is_count" -ge 5 ]; then
+        ((IS_COUNT++))
+        if [ "$IS_COUNT" -ge 5 ]; then
             printf "Error: no free port found after 5 attempts.\n" >&2 && exit 1
         fi
-        temp_port=$(shuf -i 10000-65535 -n 1)
-        [ ! "$(is_test "$temp_port")" ] && { web_port="$temp_port"; break; }
+        TEMP_PORT=$(shuf -i 10000-65535 -n 1)
+        [ ! "$(is_test "$TEMP_PORT")" ] && { WEB_PORT="$TEMP_PORT"; break; }
     done
 }
 
-cd "$workdir" || { printf "Error: Failed to enter the x-ui work directory!\n"; exit 1; }
+cd "$WORKDIR" || { printf "Error: Failed to enter the x-ui work directory!\n"; exit 1; }
 
 # For security reasons, it is necessary to mandatorily change the port and account password after installation or update.
 if [ ! -f "/etc/x-ui/x-ui.db" ]; then
     if [ -z "$USER_NAME" ] || [ -z "$USER_PASSWORD" ]; then
-        xray-ui setting -username "$usernameTemp" -password "$passwdTemp" >/dev/null 2>&1
-        printf "面板登录用户名: %s\n" "$usernameTemp" >/dev/stdout
-        printf "Panel login username: %s\n" "$usernameTemp" >/dev/stdout
-        printf "面板登录用户密码: %s\n" "$passwdTemp" >/dev/stdout
-        printf "Panel login user password: %s\n" "$passwdTemp" >/dev/stdout
+        xray-ui setting -username "$USERNAMETEMP" -password "$PASSWDTEMP" >/dev/null 2>&1
+        printf "面板登录用户名: %s\n" "$USERNAMETEMP" >/dev/stdout
+        printf "Panel login username: %s\n" "$USERNAMETEMP" >/dev/stdout
+        printf "面板登录用户密码: %s\n" "$PASSWDTEMP" >/dev/stdout
+        printf "Panel login user password: %s\n" "$PASSWDTEMP" >/dev/stdout
     fi
     if [ -z "$PANEL_PORT" ]; then
         gen_port
-        xray-ui setting -port "$web_port" >/dev/null 2>&1
-        printf "面板登录端口: %s\n" "$web_port" >/dev/stdout
-        printf "Panel login port: %s\n" "$web_port" >/dev/stdout
+        xray-ui setting -port "$WEB_PORT" >/dev/null 2>&1
+        printf "面板登录端口: %s\n" "$WEB_PORT" >/dev/stdout
+        printf "Panel login port: %s\n" "$WEB_PORT" >/dev/stdout
     fi
     if [ -n "$USER_NAME" ] && [ -n "$USER_PASSWORD" ]; then
         xray-ui setting -username "$USER_NAME" -password "$USER_PASSWORD" >/dev/null 2>&1
