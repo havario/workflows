4ff5bf0b08a18bfbe772b0f730d8d713db803cbe
honeok <yihaohey@gmail.com>
2025-05-11 14:46:11 +0800

    up

--- DanmakuRender/docker-entrypoint.sh
+++ DanmakuRender/docker-entrypoint.sh
@@ -9,16 +9,16 @@
 # Distributed on an "AS IS" basis, WITHOUT WARRANTIES.
 # See http://www.apache.org/licenses/LICENSE-2.0 for details.
 
-work_dir="/DanmakuRender"
-danmaku_config_dir="$work_dir/configs"
-danmaku_config_dir_temp="$work_dir/configs_temp"
+WORKDIR="/DanmakuRender"
+DANMAKU_CONFIG="$WORKDIR/configs"
+DANMAKU_TEMP_CONFIG="$WORKDIR/configs_temp"
 
-if [ -d "$danmaku_config_dir" ] && [ -z "$(find "$danmaku_config_dir" -mindepth 1 -print -quit)" ]; then
-    command cp -rf "$danmaku_config_dir_temp"/* "$danmaku_config_dir/"
+if [ -d "$DANMAKU_CONFIG" ] && [ -z "$(find "$DANMAKU_CONFIG" -mindepth 1 -print -quit)" ]; then
+    command cp -rf "$DANMAKU_TEMP_CONFIG"/* "$DANMAKU_CONFIG/"
 fi
 
 if [ "$#" -eq 0 ]; then
-    exec python3 -u "$work_dir/main.py" --config "$danmaku_config_dir" --skip_update
+    exec python3 -u "$WORKDIR/main.py" --config "$DANMAKU_CONFIG" --skip_update
 else
     exec "$@"
 fi
