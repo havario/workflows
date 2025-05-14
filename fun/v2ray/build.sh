81b7b62d20cfbbac37a5fbcccaadddec5afe53a4
honeok <yihaohey@gmail.com>
2025-05-14 12:45:13 +0800

    update

---
+++ fun/v2ray/build.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env sh
+#
+# Description: This script is used to build the basic operating environment for v2ray.
+#
+# Copyright (c) 2025 honeok <honeok@duck.com>
+#
+# Licensed under the GNU General Public License, version 2 only.
+# This program is distributed WITHOUT ANY WARRANTY.
+# See <https://www.gnu.org/licenses/old-licenses/gpl-2.0.html>.
+
+set -eux
+
