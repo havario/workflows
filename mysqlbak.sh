972dab8e7fa16b92f3da63becb03ec00506a6755
honeok <yihaohey@gmail.com>
2025-05-19 14:49:49 +0800

    update

--- mysqlbak.sh
+++ mysqlbak.sh
@@ -120,7 +120,7 @@ gamedb1_bak() {
         -P "${GAMEDB[MYSQL_PORT_GAMEDB1]}" \
         -u "${GAMEDB[MYSQL_USER_GAMEDB1]}" \
         -p"${GAMEDB[MYSQL_PASSWD_GAMEDB1]}" \
-        -R "$database" > "$TEMPDIR/${database}_$(LC_TIME="en_DK.UTF-8" TZ=Asia/Shanghai date +%Y.%m.%d-%H:%M:%S).sql" 2>/dev/null
+        -R "$database" > "$TEMPDIR/${database}_$(LC_TIME="en_DK.UTF-8" TZ=Asia/Shanghai date +%Y%m%d%H%M%S).sql" 2>/dev/null
         _suc_msg "$(_green "$database Backup Complete!")" || error_and_exit "$database Backup fail."
     done
     after_run "$GAMEDB_DIR"
