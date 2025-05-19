9079cf18d980dc7b83a71aaf6984e22bdf530396
honeok <yihaohey@gmail.com>
2025-05-19 12:51:16 +0800

    update

--- mysqlbak.sh
+++ mysqlbak.sh
@@ -24,11 +24,11 @@ WORKDIR_TMP="/data/dbback/tmp"
 BAK_PARAMETERS=(--no-defaults --single-transaction --set-gtid-purged=OFF)
 
 gamedb1_bak() {
-    local -A GAMEDB_1
-    local -a DATABASES_1
+    local -A GAMEDB
+    local -a DATABASES
 
     # 定义关联数组用于存储数据库连接信息, 值来自环境变量 /etc/profile.d/mysql.sh
-    GAMEDB_1=(
+    GAMEDB=(
         ["MYSQL_USER_GAMEDB1"]="$MYSQL_USER_GAMEDB1"
         ["MYSQL_PASSWD_GAMEDB1"]="$MYSQL_PASSWD_GAMEDB1"
         ["MYSQL_PORT_GAMEDB1"]="$MYSQL_PORT_GAMEDB1"
@@ -36,19 +36,19 @@ gamedb1_bak() {
     )
 
     while read -r DB; do
-        DATABASES_1+=("$DB")
-    done < <(mysql -h "${GAMEDB_1[MYSQL_IP_GAMEDB1]}" \
-            -u "${GAMEDB_1[MYSQL_USER_GAMEDB1]}" \
-            -p"${GAMEDB_1[MYSQL_PASSWD_GAMEDB1]}" \
+        DATABASES+=("$DB")
+    done < <(mysql -h "${GAMEDB[MYSQL_IP_GAMEDB1]}" \
+            -u "${GAMEDB[MYSQL_USER_GAMEDB1]}" \
+            -p"${GAMEDB[MYSQL_PASSWD_GAMEDB1]}" \
             -e "SHOW DATABASES;" 2>/dev/null | grep -Ev "(Database|information_schema|mysql|performance_schema|sys)")
 
     # 执行备份
-    for database in "${DATABASES_1[@]}"; do
+    for database in "${DATABASES[@]}"; do
         /usr/bin/mysqldump "${BAK_PARAMETERS[@]}" \
-        -h "${GAMEDB_1[MYSQL_IP_GAMEDB1]}" \
-        -P "${GAMEDB_1[MYSQL_PORT_GAMEDB1]}" \
-        -u "${GAMEDB_1[MYSQL_USER_GAMEDB1]}" \
-        -p"${GAMEDB_1[MYSQL_PASSWD_GAMEDB1]}" \
+        -h "${GAMEDB[MYSQL_IP_GAMEDB1]}" \
+        -P "${GAMEDB[MYSQL_PORT_GAMEDB1]}" \
+        -u "${GAMEDB[MYSQL_USER_GAMEDB1]}" \
+        -p"${GAMEDB[MYSQL_PASSWD_GAMEDB1]}" \
         -R "$database" > "$WORKDIR_TMP/${database}_$(LC_TIME="en_DK.UTF-8" TZ=Asia/Shanghai date +%Y.%m.%d-%H:%M:%S).sql" 2>/dev/null
         _suc_msg "$(_green "$database Backup Complete!")"
     done
