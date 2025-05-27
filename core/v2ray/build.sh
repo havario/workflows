875eddf53ce9c58ea536c2185b64caed1804fcf0
honeok <yihaohey@gmail.com>
2025-05-27 20:27:22 +0800

    update

--- core/v2ray/build.sh
+++ core/v2ray/build.sh
@@ -15,11 +15,18 @@ VERSION="$1"
 if ! git clone --depth=1 --branch "$VERSION" https://github.com/v2fly/v2ray-core.git v2ray; then
     printf 'Error: Failed to clone the project branch.\n' >&2; exit 1
 fi
+
 cd v2ray || { printf 'Error: permission denied or directory does not exist\n' >&2; exit 1; }
+
 EXTRA_ARG=""
 case "$(go env GOOS)-$(go env GOARCH)" in
     linux-amd64|linux-arm64)
         EXTRA_ARG="-buildmode=pie"
     ;;
 esac
-go build "$EXTRA_ARG" -v -trimpath -ldflags "-s -w -buildid=" -o /go/bin/v2ray ./main
+
+if [ -n "$EXTRA_ARG" ]; then
+    go build "$EXTRA_ARG" -v -trimpath -ldflags "-s -w -buildid=" -o /go/bin/v2ray ./main
+else
+    go build -v -trimpath -ldflags "-s -w -buildid=" -o /go/bin/v2ray ./main
+fi
