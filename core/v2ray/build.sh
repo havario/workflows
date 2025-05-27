a84c3391ed48e448ebe82515af32fed6fec1cc34
honeok <yihaohey@gmail.com>
2025-05-27 20:13:12 +0800

    update

--- core/v2ray/build.sh
+++ core/v2ray/build.sh
@@ -10,30 +10,16 @@
 
 set -eux
 
-build_v2ray() {
-    if ! git clone --depth=1 --branch "$VERSION" https://github.com/v2fly/v2ray-core.git v2ray; then
-        printf 'Error: Failed to clone the project branch.\n' >&2; exit 1
-    fi
-    cd v2ray || { printf 'Error: permission denied or directory does not exist\n' >&2; exit 1; }
-    EXTRA_ARG=""
-    case "$(go env GOOS)-$(go env GOARCH)" in
-        linux-amd64|linux-arm64)
-            EXTRA_ARG="-buildmode=pie"
-        ;;
-    esac
-    go build "$EXTRA_ARG" -v -trimpath -ldflags "-s -w -buildid=" -o /go/bin/v2ray ./main
-}
+VERSION="$1"
 
-case "$1" in
-    stable)
-        VERSION="$(wget -qO- --tries 5 "https://api.github.com/repos/v2fly/v2ray-core/releases/latest" | grep '"tag_name"' | cut -d '"' -f4)"
-    ;;
-    pre)
-        VERSION="$(wget -qO- --tries 5 "https://api.github.com/repos/v2fly/v2ray-core/releases" | sed -n '/"tag_name"/{s/.*: "\(.*\)".*/\1/;h}; /"prerelease": true/{g;p;q}')"
-    ;;
-    *)
-        printf 'Error: Unable to determine v2ray version!\n' >&2; exit 1
+if ! git clone --depth=1 --branch "$VERSION" https://github.com/v2fly/v2ray-core.git v2ray; then
+    printf 'Error: Failed to clone the project branch.\n' >&2; exit 1
+fi
+cd v2ray || { printf 'Error: permission denied or directory does not exist\n' >&2; exit 1; }
+EXTRA_ARG=""
+case "$(go env GOOS)-$(go env GOARCH)" in
+    linux-amd64|linux-arm64)
+        EXTRA_ARG="-buildmode=pie"
     ;;
 esac
-
-build_v2ray
+go build "$EXTRA_ARG" -v -trimpath -ldflags "-s -w -buildid=" -o /go/bin/v2ray ./main
