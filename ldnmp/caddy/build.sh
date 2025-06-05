94b71fab01ced33bd8f606aacc0685c4dfc82ef4
honeok <yihaohey@gmail.com>
2025-06-05 17:11:07 +0800

    up

--- ldnmp/caddy/build.sh
+++ ldnmp/caddy/build.sh
@@ -2,6 +2,6 @@
 
 VERSION="$(wget -qO- --tries=5 "https://api.github.com/repos/caddyserver/caddy/releases/latest" | awk -F '["v]' '/tag_name/{print $5}')"
 
-docker build --no-cache --build-arg "VERSION=$VERSION" -t honeok/caddy:dev .
+docker build --no-cache --build-arg "VERSION=$VERSION" -t "honeok/caddy:$VERSION" .
 
 docker system prune -af --volumes
