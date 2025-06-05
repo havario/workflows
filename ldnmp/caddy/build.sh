693b5d3eb7404aff951188335b56a14bc695188e
havario <157877551+havario@users.noreply.github.com>
2025-06-05 17:24:16 +0800

    up

--- ldnmp/caddy/build.sh
+++ ldnmp/caddy/build.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
+#
+# Copyright (c) 2025 honeok <honeok@autistici.org>
+#
+# SPDX-License-Identifier: Apache License 2.0
 
 VERSION="$(wget -qO- --tries=5 "https://api.github.com/repos/caddyserver/caddy/releases/latest" | awk -F '["v]' '/tag_name/{print $5}')"
 
 docker build --no-cache --build-arg "VERSION=$VERSION" -t "honeok/caddy:$VERSION" .
+
+docker run -d --name caddy --network host "honeok/caddy:$VERSION"
 
 docker system prune -af --volumes
