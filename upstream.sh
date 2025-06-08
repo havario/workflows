db4708c7302bb7aa3fb9dc8e0a42821281b1b991
havario <157877551+havario@users.noreply.github.com>
2025-06-08 16:30:00 +0800

    up

--- upstream.sh
+++ upstream.sh
@@ -2,11 +2,9 @@
 #
 # Description: This script is used to set the upstream repository commit user.
 #
-# Copyright (c) 2025 honeok <honeok@autistici.org>
+# Copyright (c) 2025 honeok <honeok@disroot.org>
 #
-# Licensed under the GNU General Public License, version 2 only.
-# This program is distributed WITHOUT ANY WARRANTY.
-# See <https://www.gnu.org/licenses/old-licenses/gpl-2.0.html>.
+# SPDX-License-Identifier: GPL-2.0-only
 
 set -e
 
@@ -16,7 +14,7 @@ CODE_PLATFORM="$(pwd | awk -F'/' '{print $(NF-1)}')"
 case "$CODE_PLATFORM" in
     github*) WORK_PLATFORM="github" ;;
     gitlab*) WORK_PLATFORM="gitlab" ;;
-    *) echo "Error: Unknown platform." && exit 1 ;;
+    *) echo "Error: Unknown platform."; exit 1 ;;
 esac
 
 read -rep "Please enter user: " USER
@@ -25,7 +23,6 @@ case "$USER" in
         git config user.name "$USER"
         [ "$WORK_PLATFORM" = "github" ] && git config user.email "100125733+honeok@users.noreply.github.com"
         git remote set-url origin "git@${WORK_PLATFORM}-${USER}:${USER}/${REPOSITORY}.git"
-        echo "----------"
         git config --get user.name
         git config --get user.email
     ;;
@@ -33,11 +30,10 @@ case "$USER" in
         git config user.name "$USER"
         [ "$WORK_PLATFORM" = "github" ] && git config user.email "157877551+havario@users.noreply.github.com"
         git remote set-url origin "git@${WORK_PLATFORM}-${USER}:${USER}/${REPOSITORY}.git"
-        echo "----------"
         git config --get user.name
         git config --get user.email
     ;;
     *)
-        echo "Error: Unknown User" && exit 1
+        echo "Error: Unknown User"; exit 1
     ;;
 esac
