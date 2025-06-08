b4201f5a95762e47a6b6a223e7387c3bffc1b9cc
havario <157877551+havario@users.noreply.github.com>
2025-06-08 16:43:51 +0800

    up

--- upstream.sh
+++ upstream.sh
@@ -10,6 +10,7 @@ set -e
 
 REPOSITORY="$(pwd | awk -F'/' '{print $NF}')"
 CODE_PLATFORM="$(pwd | awk -F'/' '{print $(NF-1)}')"
+separator() { printf "%-20s\n" "-" | sed 's/\s/-/g'; }
 
 case "$CODE_PLATFORM" in
     github*) WORK_PLATFORM="github" ;;
@@ -17,23 +18,30 @@ case "$CODE_PLATFORM" in
     *) echo "Error: Unknown platform."; exit 1 ;;
 esac
 
-read -rep "Please enter user: " USER
-case "$USER" in
-    honeok)
-        git config user.name "$USER"
-        [ "$WORK_PLATFORM" = "github" ] && git config user.email "100125733+honeok@users.noreply.github.com"
-        git remote set-url origin "git@${WORK_PLATFORM}-${USER}:${USER}/${REPOSITORY}.git"
-        git config --get user.name
-        git config --get user.email
-    ;;
-    havario)
-        git config user.name "$USER"
-        [ "$WORK_PLATFORM" = "github" ] && git config user.email "157877551+havario@users.noreply.github.com"
-        git remote set-url origin "git@${WORK_PLATFORM}-${USER}:${USER}/${REPOSITORY}.git"
-        git config --get user.name
-        git config --get user.email
-    ;;
-    *)
-        echo "Error: Unknown User"; exit 1
-    ;;
-esac
+while true; do
+    separator
+    echo " 1. honeok"
+    echo " 2. havario"
+    separator
+    read -rep "Please enter user: " USER
+    case "$USER" in
+        1)
+            git config user.name honeok
+            [ "$WORK_PLATFORM" = "github" ] && git config user.email "100125733+honeok@users.noreply.github.com"
+            git remote set-url origin "git@${WORK_PLATFORM}-${USER}:${USER}/${REPOSITORY}.git"
+            break
+        ;;
+        2)
+            git config user.name havario
+            [ "$WORK_PLATFORM" = "github" ] && git config user.email "157877551+havario@users.noreply.github.com"
+            git remote set-url origin "git@${WORK_PLATFORM}-${USER}:${USER}/${REPOSITORY}.git"
+            break
+        ;;
+        *)
+            echo "Error: Unknown User"
+        ;;
+    esac
+
+    git config --get user.name
+    git config --get user.email
+done
