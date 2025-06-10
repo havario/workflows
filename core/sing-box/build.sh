f1805eeb38db862e258b810fd6cec78392fc7fdb
havario <157877551+havario@users.noreply.github.com>
2025-06-11 01:28:48 +0800

    up

--- core/sing-box/build.sh
+++ core/sing-box/build.sh
@@ -18,18 +18,15 @@ SINGBOX_LOGFILE="$SINGBOX_LOGDIR/access.log"
 
 command -v curl >/dev/null 2>&1 || apk add --no-cache curl
 
-echo "DEBUG: Combined is $(uname -m)"
-
 # Determine system arch based
 case "$TARGETOS/$TARGETARCH" in
     linux/386 ) OS_ARCH="386" ;;
     linux/amd64 ) OS_ARCH="amd64" ;;
-    linux/arm/v6 ) OS_ARCH="armv6" ;;
-    linux/arm/v7 ) OS_ARCH="armv7" ;;
     linux/arm64 | linux/arm64/v8 ) OS_ARCH="arm64" ;;
+    linux/arm* ) [ "$(uname -m)" = "armv6*" ] && OS_ARCH="armv6" || [ "$(uname -m)" = "armv7*" ] && OS_ARCH="armv7" || { printf "Error: unsupported arm architecture: %s\n" "$(uname -m)" >&2; exit 1; } ;;
     linux/ppc64le ) OS_ARCH="ppc64le" ;;
     linux/riscv64 ) OS_ARCH="riscv64" ;;
-    linux/s390x) OS_ARCH="s390x" ;;
+    linux/s390x ) OS_ARCH="s390x" ;;
     * ) printf "Error: unsupported architecture: %s\n" "$TARGETARCH" >&2; exit 1 ;;
 esac
 
@@ -37,12 +34,12 @@ esac
 mkdir -p "$SINGBOX_WORKDIR" "$SINGBOX_BINDIR" "$SINGBOX_CONFDIR" "$SINGBOX_LOGDIR" >/dev/null 2>&1
 touch "$SINGBOX_LOGFILE" >/dev/null 2>&1
 
-cd "$SINGBOX_BINDIR" || { printf "Error: Failed to enter the sing-box bin directory!\n"; exit 1; }
+cd "$SINGBOX_BINDIR" || { printf "Error: Failed to enter the sing-box bin directory!\n" >&2; exit 1; }
 # Extract and install Sing-Box
 if ! curl -fsL -O "https://github.com/SagerNet/sing-box/releases/download/v${SINGBOX_LVER}/sing-box-${SINGBOX_LVER}-${TARGETOS}-${OS_ARCH}.tar.gz"; then
-    printf "Error: Download sing-Box failed, please check the network!\n" >&2; exit 1
+    printf "Error: Download Sing-Box failed, please check the network!\n" >&2; exit 1
 fi
-tar -zxf "sing-box-${SINGBOX_LVER}-${TARGETOS}-${OS_ARCH}.tar.gz" --strip-components=1 || { printf "Error: tar Sing-box package failed!\n"; exit 1; }
+tar -zxf "sing-box-${SINGBOX_LVER}-${TARGETOS}-${OS_ARCH}.tar.gz" --strip-components=1 || { printf "Error: tar sing-box package failed!\n" >&2; exit 1; }
 find . -mindepth 1 -maxdepth 1 ! -name 'sing-box' -exec rm -rf {} +
 [ ! -x "$SINGBOX_BINDIR/sing-box" ] && chmod +x "$SINGBOX_BINDIR/sing-box"
-ln -s "$SINGBOX_BINDIR/sing-box" /usr/local/bin/sing-box
+ln -sf "$SINGBOX_BINDIR/sing-box" /usr/local/bin/sing-box
