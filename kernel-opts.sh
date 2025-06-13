6d6acfc315bf37f898184ff7937140b0fd84f27f
havario <157877551+havario@users.noreply.github.com>
2025-06-13 12:49:38 +0800

    up

--- kernel-opts.sh
+++ kernel-opts.sh
@@ -13,6 +13,8 @@
 
 # 当前脚本版本号
 readonly VERSION='v1.1.1 (2025.06.13)'
+# shellcheck disable=SC2034
+readonly SCRIPT_SHA='d811b29ed0b540970cdb44b3d0fc272de8eca13f'
 
 # 环境变量用于在debian或ubuntu操作系统中设置非交互式 (noninteractive) 安装模式
 export DEBIAN_FRONTEND=noninteractive
