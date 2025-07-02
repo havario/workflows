5b6b5bfe53dd0a727f9ca706ff8624c643dec22c
havario <157877551+havario@users.noreply.github.com>
2025-07-03 00:37:43 +0800

    zstd

--- ldnmp/tengine/build.sh
+++ ldnmp/tengine/build.sh
@@ -7,7 +7,10 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 
+set -e
+
 TENGINE_LVER="$(wget -qO- --tries=50 https://api.github.com/repos/alibaba/tengine/releases | sed -n 's/.*"tag_name": *"\(tengine-\|\)\([^"]*\)".*/\2/p' | sort -Vr | head -n1)"
+ZSTD_LVER="$(wget -qO- --tries=50 https://api.github.com/repos/facebook/zstd/releases/latest | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/' | sed 's/v//')"
 HEADERSMORE_LVER="$(wget -qO- --tries=50 https://api.github.com/repos/openresty/headers-more-nginx-module/tags | sed -n 's/.*"name": *"v\{0,1\}\([^"]*\)".*/\1/p' | grep -v 'rc' | sort -Vr | head -n1)"
 # TONGSUO_LVER="$(wget -qO- --tries=50 https://api.github.com/repos/Tongsuo-Project/Tongsuo/tags | sed -n 's/.*"name": *"\([^"]*\)".*/\1/p' | grep -v 'pre' | sort -Vr | head -n1)"
 # XQUIC_LVER="$(wget -qO- --tries=50 https://api.github.com/repos/alibaba/xquic/releases | sed -n 's/.*"tag_name": *"\(v[^"]*\)".*/\1/p' | sort -Vr | head -n1 | sed 's/^v//')"
@@ -20,18 +23,11 @@ _exit() {
 
 trap '_exit' SIGINT SIGQUIT SIGTERM EXIT
 
-# docker build --no-cache \
-#     --progress=plain \
-#     --build-arg TENG_LVER="$TENGINE_LVER" \
-#     --build-arg MORE_LVER="$HEADERSMORE_LVER" \
-#     --build-arg TONGSUO_LVER="$TONGSUO_LVER" \
-#     --build-arg XQUIC_LVER="$XQUIC_LVER" \
-#     -t honeok/tengine:"$TENGINE_LVER" .
-
 # stable tongsuo and xquic version
 docker build --no-cache \
     --progress=plain \
     --build-arg TENG_LVER="$TENGINE_LVER" \
+    --build-arg ZSTD_LVER="$ZSTD_LVER" \
     --build-arg MORE_LVER="$HEADERSMORE_LVER" \
     -t honeok/tengine:"$TENGINE_LVER" .
 
