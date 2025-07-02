6c2be8651b93e7ba66f6314aeb2b0f42e53031ff
havario <157877551+havario@users.noreply.github.com>
2025-07-02 10:28:43 +0800

    up

--- ldnmp/xquic/build.sh
+++ ldnmp/xquic/build.sh
@@ -10,9 +10,11 @@
 TENGINE_LVER="$(wget -qO- --tries=50 https://api.github.com/repos/alibaba/tengine/releases | sed -n 's/.*"tag_name": *"\(tengine-\|\)\([^"]*\)".*/\2/p' | sort -Vr | head -n1)"
 HEADERSMORE_LVER="$(wget -qO- --tries=50 https://api.github.com/repos/openresty/headers-more-nginx-module/tags | sed -n 's/.*"name": *"v\{0,1\}\([^"]*\)".*/\1/p' | grep -v 'rc' | sort -Vr | head -n1)"
 TONGSUO_LVER="$(wget -qO- --tries=50 https://api.github.com/repos/Tongsuo-Project/Tongsuo/tags | sed -n 's/.*"name": *"\([^"]*\)".*/\1/p' | grep -v 'pre' | sort -Vr | head -n1)"
+XQUIC_LVER="$(wget -qO- --tries=50 https://api.github.com/repos/alibaba/xquic/releases | sed -n 's/.*"tag_name": *"\(v[^"]*\)".*/\1/p' | sort -Vr | head -n1 | sed 's/^v//')"
 
 docker build --no-cache \
     --build-arg TENG_LVER="$TENGINE_LVER" \
     --build-arg MORE_LVER="$HEADERSMORE_LVER" \
     --build-arg TONGSUO_LVER="$TONGSUO_LVER" \
+    --build-arg XQUIC_LVER="$XQUIC_LVER" \
     -t honeok/tengine:"$TENGINE_LVER" .
