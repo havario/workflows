c8817eaf9d65bc11721354e5344aa6609faf542e
havario <157877551+havario@users.noreply.github.com>
2025-07-05 13:53:30 +0800

    build

---
+++ ldnmp/openresty/luarocks/build.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+
+set -eE
+
+START_TIME="$(date +%s)"
+
+_exit() {
+    local ET_CODE="$?"
+    docker buildx prune --all --force 2>/dev/null
+    docker system prune -af --volumes 2>/dev/null
+    docker buildx rm -f builder 2>/dev/null
+    echo 2>&1 "Total execution time: $MINUTES m $SECONDS s"
+    exit "$ET_CODE"
+}
+
+trap '_exit' SIGINT SIGQUIT SIGTERM EXIT
+
+docker buildx create --name builder --use
+docker buildx inspect --bootstrap
+
+# If the docker-entrypoint script does not have permissions.
+find ./ -type f -name "*.sh" -exec dos2unix {} \; -exec chmod +x {} \;
+
+docker buildx build \
+    --no-cache \
+    --progress=plain \
+    --platform linux/amd64,linux/arm64/v8 \
+    --tag honeok/openresty:"$RESTY_VERSION-alpine-fat" \
+    --push \
+    . && echo 2>&1 "build complete!"
+
+END_TIME="$(date +%s)"
+DURATION=$(( END_TIME - START_TIME ))
+MINUTES=$(( DURATION / 60 ))
+SECONDS=$(( DURATION % 60 ))
