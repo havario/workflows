ec5490b0e46fc43cf0fe0e16d26be0ec0c9a5781
havario <157877551+havario@users.noreply.github.com>
2025-08-30 18:51:11 +0800

    status
    Signed-off-by: havario <157877551+havario@users.noreply.github.com>

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 
-# common
+## common
 sed -i 's#https://unpkg.com/jquery@3.7.1/dist/jquery.min.js#https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js#g' resource/template/common/footer.html
 sed -i 's#https://unpkg.com/semantic-ui@2.4.0/dist/semantic.min.js#https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.4.0/semantic.min.js#g' resource/template/common/footer.html
 sed -i 's#https://unpkg.com/vue@2.6.14/dist/vue.min.js#https://cdnjs.cloudflare.com/ajax/libs/vue/2.6.14/vue.min.js#g' resource/template/common/footer.html
@@ -14,7 +14,7 @@ sed -i 's#https://unpkg.com/vue@2.6.14/dist/vue.min.js#https://cdnjs.cloudflare.
 sed -i 's#https://unpkg.com/semantic-ui@2.4.0/dist/semantic.min.css#https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.4.0/semantic.min.css#g' resource/template/common/header.html
 sed -i 's#https://unpkg.com/font-logos@0.17.0/assets/font-logos.css#https://registry.npmmirror.com/font-logos/0.17.0/files/assets/font-logos.css#g' resource/template/common/header.html
 
-# dashboard-default
+## dashboard-default
 sed -i 's#https://unpkg.com/mdui@2/mdui.css#https://cdnjs.cloudflare.com/ajax/libs/mdui/2.1.4/mdui.min.css#g' resource/template/dashboard-default/file.html
 sed -i 's#https://unpkg.com/mdui@2/mdui.global.js#https://cdnjs.cloudflare.com/ajax/libs/mdui/2.1.4/mdui.global.min.js#g' resource/template/dashboard-default/file.html
 
@@ -26,7 +26,7 @@ sed -i 's#https://unpkg.com/@xterm/addon-fit@0.10.0/lib/addon-fit.js#https://reg
 sed -i 's#https://unpkg.com/@xterm/addon-web-links@0.11.0/lib/addon-web-links.js#https://registry.npmmirror.com/@xterm/addon-web-links/0.11.0/files/lib/addon-web-links.js#g' resource/template/dashboard-default/terminal.html
 sed -i 's#https://unpkg.com/@xterm/addon-attach@0.11.0/lib/addon-attach.js#https://registry.npmmirror.com/@xterm/addon-attach/0.11.0/files/lib/addon-attach.js#g' resource/template/dashboard-default/terminal.html
 
-# theme-angel-kanade
+## theme-angel-kanade
 sed -i 's#https://lf6-cdn-tos.bytecdntp.com/cdn/expire-1-y/jquery/3.6.0/jquery.min.js#https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js#g' resource/template/theme-angel-kanade/footer.html
 sed -i 's#https://lf6-cdn-tos.bytecdntp.com/cdn/expire-1-y/semantic-ui/2.4.1/semantic.min.js#https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.4.1/semantic.min.js#g' resource/template/theme-angel-kanade/footer.html
 sed -i 's#https://lf6-cdn-tos.bytecdntp.com/cdn/expire-1-y/vue/2.6.14/vue.min.js#https://cdnjs.cloudflare.com/ajax/libs/vue/2.6.14/vue.min.js#g' resource/template/theme-angel-kanade/footer.html
@@ -37,7 +37,7 @@ sed -i 's#https://lf6-cdn-tos.bytecdntp.com/cdn/expire-1-y/bootstrap-icons/1.8.1
 
 sed -i 's#https://lf6-cdn-tos.bytecdntp.com/cdn/expire-1-y/flag-icon-css/4.1.5/flags/1x1/#https://cdnjs.cloudflare.com/ajax/libs/flag-icon-css/4.1.5/flags/1x1/#g' resource/template/theme-angel-kanade/home.html
 
-# theme-daynight
+## theme-daynight
 sed -i 's#https://lf6-cdn-tos.bytecdntp.com/cdn/expire-1-y/font-awesome/6.0.0/css/all.min.css#https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css#g' resource/template/theme-daynight/home.html
 sed -i 's#https://lf6-cdn-tos.bytecdntp.com/cdn/expire-1-y/jquery/3.6.0/jquery.min.js#https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js#g' resource/template/theme-daynight/home.html
 sed -i 's#https://lf6-cdn-tos.bytecdntp.com/cdn/expire-1-y/vue/2.6.14/vue.min.js#https://cdnjs.cloudflare.com/ajax/libs/vue/2.6.14/vue.min.js#g' resource/template/theme-daynight/home.html
@@ -53,7 +53,7 @@ sed -i 's#https://lf6-cdn-tos.bytecdntp.com/cdn/expire-1-y/mdui/1.0.2/css/mdui.m
 sed -i 's#https://lf6-cdn-tos.bytecdntp.com/cdn/expire-1-y/font-awesome/6.0.0/css/all.min.css#https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css#g' resource/template/theme-daynight/service.html
 sed -i 's#https://lf6-cdn-tos.bytecdntp.com/cdn/expire-1-y/jquery/3.6.0/jquery.min.js#https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js#g' resource/template/theme-daynight/service.html
 
-# theme-default
+## theme-default
 sed -i 's#https://unpkg.com/semantic-ui@2.4.0/dist/semantic.min.css#https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.4.0/semantic.min.css#g' resource/template/theme-default/header.html
 sed -i 's#https://unpkg.com/font-logos@0.17.0/assets/font-logos.css#https://cdn.jsdelivr.net/npm/font-logos@0.17.0/assets/font-logos.css#g' resource/template/theme-default/header.html
 sed -i 's#https://unpkg.com/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css#https://cdnjs.cloudflare.com/ajax/libs/bootstrap-icons/1.11.3/font/bootstrap-icons.min.css#g' resource/template/theme-default/header.html
@@ -63,11 +63,34 @@ sed -i 's#https://unpkg.com/semantic-ui@2.4.0/dist/semantic.min.js#https://cdnjs
 sed -i 's#https://unpkg.com/vue@2.6.14/dist/vue.min.js#https://cdnjs.cloudflare.com/ajax/libs/vue/2.6.14/vue.min.js#g' resource/template/theme-default/header.html
 sed -i 's#https://unpkg.com/echarts@5.5.0/dist/echarts.min.js#https://cdnjs.cloudflare.com/ajax/libs/echarts/5.5.0/echarts.min.js#g' resource/template/theme-default/header.html
 
-# theme-hotaru
+## theme-hotaru
 sed -i 's#https://lf6-cdn-tos.bytecdntp.com/cdn/expire-1-y/semantic-ui/2.4.1/semantic.min.css#https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.4.1/semantic.min.css#g' resource/template/theme-hotaru/home.html
 sed -i 's#https://lf6-cdn-tos.bytecdntp.com/cdn/expire-1-y/font-awesome/6.0.0/css/all.min.css#https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css#g' resource/template/theme-hotaru/home.html
 sed -i 's#https://lf6-cdn-tos.bytecdntp.com/cdn/expire-1-y/jquery/3.6.0/jquery.min.js#https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js#g' resource/template/theme-hotaru/home.html
 sed -i 's#https://lf6-cdn-tos.bytecdntp.com/cdn/expire-1-y/semantic-ui/2.4.1/semantic.min.js#https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.4.1/semantic.min.js#g' resource/template/theme-hotaru/home.html
 sed -i 's#https://lf6-cdn-tos.bytecdntp.com/cdn/expire-1-y/vue/2.6.14/vue.min.js#https://cdnjs.cloudflare.com/ajax/libs/vue/2.6.14/vue.min.js#g' resource/template/theme-hotaru/home.html
 
-# theme-mdui
+## theme-mdui
+sed -i 's#https://lf6-cdn-tos.bytecdntp.com/cdn/expire-1-y/mdui/1.0.2/css/mdui.min.css#https://cdnjs.cloudflare.com/ajax/libs/mdui/1.0.2/css/mdui.min.css#g' resource/template/theme-mdui/home.html
+sed -i 's#https://lf6-cdn-tos.bytecdntp.com/cdn/expire-1-y/flag-icon-css/4.1.5/flags/1x1/#https://cdnjs.cloudflare.com/ajax/libs/flag-icon-css/4.1.5/flags/1x1/#g' resource/template/theme-mdui/home.html
+sed -i 's#https://lf6-cdn-tos.bytecdntp.com/cdn/expire-1-y/mdui/1.0.2/js/mdui.min.js#https://cdnjs.cloudflare.com/ajax/libs/mdui/1.0.2/js/mdui.min.js#g' resource/template/theme-mdui/home.html
+sed -i 's#https://lf6-cdn-tos.bytecdntp.com/cdn/expire-1-y/jquery/3.6.0/jquery.min.js#https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js#g' resource/template/theme-mdui/home.html
+sed -i 's#https://lf6-cdn-tos.bytecdntp.com/cdn/expire-1-y/vue/2.6.14/vue.min.js#https://cdnjs.cloudflare.com/ajax/libs/vue/2.6.14/vue.min.js#g' resource/template/theme-mdui/home.html
+
+sed -i 's#https://lf6-cdn-tos.bytecdntp.com/cdn/expire-1-y/mdui/1.0.2/css/mdui.min.css#https://cdnjs.cloudflare.com/ajax/libs/mdui/1.0.2/css/mdui.min.css#g' resource/template/theme-mdui/network.html
+sed -i 's#https://lf6-cdn-tos.bytecdntp.com/cdn/expire-1-y/flag-icon-css/4.1.5/flags/4x3/#https://cdnjs.cloudflare.com/ajax/libs/flag-icon-css/4.1.5/flags/4x3/#g' resource/template/theme-mdui/network.html
+sed -i 's#https://lf6-cdn-tos.bytecdntp.com/cdn/expire-1-y/mdui/1.0.2/js/mdui.min.js#https://cdnjs.cloudflare.com/ajax/libs/mdui/1.0.2/js/mdui.min.js#g' resource/template/theme-mdui/network.html
+sed -i 's#https://lf6-cdn-tos.bytecdntp.com/cdn/expire-1-y/jquery/3.6.0/jquery.min.js#https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js#g' resource/template/theme-mdui/network.html
+sed -i 's#https://lf6-cdn-tos.bytecdntp.com/cdn/expire-1-y/vue/2.6.14/vue.min.js#https://cdnjs.cloudflare.com/ajax/libs/vue/2.6.14/vue.min.js#g' resource/template/theme-mdui/network.html
+sed -i 's#https://lf26-cdn-tos.bytecdntp.com/cdn/expire-1-M/echarts/5.3.0-rc.1/echarts.min.js#https://cdnjs.cloudflare.com/ajax/libs/echarts/5.3.0-rc.1/echarts.min.js#g' resource/template/theme-mdui/network.html
+
+sed -i 's#https://lf6-cdn-tos.bytecdntp.com/cdn/expire-1-y/mdui/1.0.2/css/mdui.min.css#https://cdnjs.cloudflare.com/ajax/libs/mdui/1.0.2/css/mdui.min.css#g' resource/template/theme-mdui/service.html
+sed -i 's#https://lf6-cdn-tos.bytecdntp.com/cdn/expire-1-y/mdui/1.0.2/js/mdui.min.js#https://cdnjs.cloudflare.com/ajax/libs/mdui/1.0.2/js/mdui.min.js#g' resource/template/theme-mdui/service.html
+sed -i 's#https://lf6-cdn-tos.bytecdntp.com/cdn/expire-1-y/jquery/3.6.0/jquery.min.js#https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js#g' resource/template/theme-mdui/service.html
+sed -i 's#https://lf6-cdn-tos.bytecdntp.com/cdn/expire-1-y/vue/2.6.14/vue.min.js#https://cdnjs.cloudflare.com/ajax/libs/vue/2.6.14/vue.min.js#g' resource/template/theme-mdui/service.html
+
+sed -i 's#https://lf6-cdn-tos.bytecdntp.com/cdn/expire-1-y/mdui/1.0.2/css/mdui.min.css#https://cdnjs.cloudflare.com/ajax/libs/mdui/1.0.2/css/mdui.min.css#g' resource/template/theme-mdui/viewpassword.html
+sed -i 's#https://lf6-cdn-tos.bytecdntp.com/cdn/expire-1-y/mdui/1.0.2/js/mdui.min.js#https://cdnjs.cloudflare.com/ajax/libs/mdui/1.0.2/js/mdui.min.js#g' resource/template/theme-mdui/viewpassword.html
+sed -i 's#https://lf6-cdn-tos.bytecdntp.com/cdn/expire-1-y/jquery/3.6.0/jquery.min.js#https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js#g' resource/template/theme-mdui/viewpassword.html
+
+## theme-server-status
