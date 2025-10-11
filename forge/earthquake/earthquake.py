22a80dd11945c4bf89aba4aa7d26b9ae229a3e21
havario <157877551+havario@users.noreply.github.com>
2025-10-11 20:27:32 +0800

    gunicorn
    Signed-off-by: havario <157877551+havario@users.noreply.github.com>

--- forge/earthquake/earthquake.py
+++ forge/earthquake/earthquake.py
@@ -45,9 +45,3 @@ def get_earthquakes():
         return jsonify({'earthquakes': processed})
     except Exception as e:
         return jsonify({'error': str(e)}), 500
-
-# if __name__ == '__main__':
-#     app.run(debug=True, host='0.0.0.0', port=5000)
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
