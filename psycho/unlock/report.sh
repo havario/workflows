df564a9eceec316ea5e91ff9b1c65555dbc4241b
havario <157877551+havario@users.noreply.github.com>
2025-10-15 20:49:41 +0800

    copy
    Signed-off-by: havario <157877551+havario@users.noreply.github.com>

--- psycho/unlock/report.sh
+++ psycho/unlock/report.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 # SPDX-License-Identifier: GPL-2.0
 #
-# Copyright (c) 2025 honeok <i@honeok.com>
 # Description:
+# Copyright (c) 2025 honeok <i@honeok.com>
 
 echo 0 | bash <(curl -Ls https://github.com/lmc999/RegionRestrictionCheck/raw/main/check.sh | sed 's/^showGoodbye$/# showGoodbye/')
