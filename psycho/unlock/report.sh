62b4b57b50a49a9d6e87c485eed3fffae01b1041
havario <157877551+havario@users.noreply.github.com>
2025-10-16 14:01:38 +0800

    echo
    Signed-off-by: havario <157877551+havario@users.noreply.github.com>

--- psycho/unlock/report.sh
+++ psycho/unlock/report.sh
@@ -4,4 +4,8 @@
 # Description:
 # Copyright (c) 2025 honeok <i@honeok.com>
 
-echo 0 | bash <(curl -Ls https://github.com/lmc999/RegionRestrictionCheck/raw/main/check.sh | sed 's/^showGoodbye$/# showGoodbye/')
+set -eEu
+
+UNLOCK_RESULT="$(bash <(curl -Ls https://github.com/lmc999/RegionRestrictionCheck/raw/main/check.sh | sed 's/^showGoodbye$/# showGoodbye/') <<< "0")"
+CLEAN_RESULT="$(echo "$UNLOCK_RESULT" | sed -n '1h;1!H;${g;s#.*https://t.me/gameaccelerate##p}')"
+echo "$CLEAN_RESULT"
