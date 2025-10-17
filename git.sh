aa02d38b043791e05f4ae9575587f1896d3113a2
honeok <i@honeok.com>
2025-10-17 10:24:27 +0800

    update

--- git.sh
+++ git.sh
@@ -2,6 +2,16 @@
 
 set -eEu
 
+PLATFORM="$(pwd | awk -F'/' '{print $(NF-1)}')"
+REMOTE_ORIGIN="$(git config --get remote.origin.url)"
+separator() { printf "%-20s\n" "-" | sed 's/\s/-/g'; }
+
+case "$PLATFORM" in
+    github*) WORK_PLATFORM="github" ;;
+    gitlab*) WORK_PLATFORM="gitlab" ;;
+    *) echo "Error: Unknown platform." && exit 1 ;;
+esac
+
 # global variable
 if ! git config --global --get-regexp url | grep -Fx "url.ssh://git@ssh.github.com:443/.insteadof git@github.com:" >/dev/null 2>&1; then
     git config --global url."ssh://git@ssh.github.com:443/".insteadof git@github.com:
@@ -9,7 +19,31 @@ if ! git config --global --get-regexp url | grep -Fx "url.ssh://git@ssh.github.c
 fi
 
 # set user
-git config user.name havario
-git config user.email "157877551+havario@users.noreply.github.com"
+while true; do
+    separator
+    echo " 1. honeok"
+    echo " 2. havario"
+    separator
+    read -rep "Please enter user: " USER
+    case "$USER" in
+        1)
+            git config user.name honeok
+            [ "$WORK_PLATFORM" = "github" ] && git config user.email "100125733+honeok@users.noreply.github.com"
+            git remote set-url origin "$REMOTE_ORIGIN"
+            break
+        ;;
+        2)
+            git config user.name havario
+            [ "$WORK_PLATFORM" = "github" ] && git config user.email "157877551+havario@users.noreply.github.com"
+            git remote set-url origin "$REMOTE_ORIGIN"
+            break
+        ;;
+        *)
+            echo "Error: Unknown User"
+        ;;
+    esac
+done
 
-git remote set-url origin "$(git config --get remote.origin.url)"
+separator
+git config --get user.name
+git config --get user.email
