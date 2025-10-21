d8d1080d7f23496e5c60f626e09f7c4dd965b432
honeok <i@honeok.com>
2025-10-22 01:24:45 +0800

    mv
    Signed-off-by: honeok <i@honeok.com>

--- webserver/script/rotate.sh
+++ webserver/script/rotate.sh
@@ -2,10 +2,12 @@
 
 set -eEuo pipefail
 
+# 各变量默认值
 SCRIPT_DIR="$(dirname "${BASH_SOURCE:-$0}")"
 BOT_TOKEN=""
 CHAT_ID=""
 BARK_TOKEN=""
+START_TIME="$(date +%Y-%m-%d)"
 
 die() {
     echo >&2 "Error: $*"; exit 1
@@ -39,3 +41,11 @@ send_msg() {
         curl -Ls "https://api.honeok.de/$BARK_TOKEN/Nginx/$MESSAGE" >/dev/null 2>&1
     fi
 }
+
+cd "${SCRIPT_DIR:?}" || die "Unable to enter directory."
+mv -f logs/access.log "logs/access_$START_TIME.log" >/dev/null 2>&1
+mv -f logs/error.log "logs/error_$START_TIME.log" >/dev/null 2>&1
+docker exec nginx nginx -s reopen >/dev/null 2>&1
+gzip "logs/access_$START_TIME.log" >/dev/null 2>&1
+gzip "logs/error_$START_TIME.log" >/dev/null 2>&1
+find logs -type f -name "*.log.gz" -mtime +7 -exec rm {} \; >/dev/null 2>&1
