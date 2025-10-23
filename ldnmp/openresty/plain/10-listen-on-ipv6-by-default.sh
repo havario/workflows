2ac7b6e3f73a3cb4d5ea3c88455e0438eaaf6311
havario <157877551+havario@users.noreply.github.com>
2025-10-23 13:44:22 +0800

    modified
    Signed-off-by: havario <157877551+havario@users.noreply.github.com>

--- ldnmp/openresty/plain/10-listen-on-ipv6-by-default.sh
+++ ldnmp/openresty/plain/10-listen-on-ipv6-by-default.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 # vim:sw=4:ts=4:et
 # SPDX-License-Identifier: BSD-2-Clause
+#
+# Modified from the project: https://github.com/nginx/docker-nginx
+# Copyright (c) 2025 honeok <i@honeok.com>
 
 set -e
 
