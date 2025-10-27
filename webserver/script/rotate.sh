4bcf1d78ce9e7551edf4d3de6a4fd9b84cbb686a
havario <157877551+havario@users.noreply.github.com>
2025-10-27 10:51:34 +0800

    up
    Signed-off-by: havario <157877551+havario@users.noreply.github.com>

--- webserver/script/rotate.sh
+++ webserver/script/rotate.sh
@@ -3,11 +3,15 @@
 set -eEuo pipefail
 
 # 各变量默认值
-SCRIPT_DIR="$(dirname "${BASH_SOURCE:-$0}")"
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]:-$0}")" && pwd)"
 BOT_TOKEN=""
 CHAT_ID=""
 BARK_TOKEN=""
-START_TIME="$(date +%Y-%m-%d)"
+
+# 设置PATH环境变量
+export PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:$PATH
+# 环境变量用于在debian或ubuntu操作系统中设置非交互式 (noninteractive) 安装模式
+export DEBIAN_FRONTEND=noninteractive
 
 die() {
     echo >&2 "Error: $*"; exit 1
@@ -29,6 +33,56 @@ curl() {
     done
 }
 
+check_root() {
+    if [ "$EUID" -ne 0 ] || [ "$(id -ru)" -ne 0 ]; then
+        die "This script must be run as root."
+    fi
+}
+
+check_cmd() {
+    local -a INSTALL_PKG
+    INSTALL_PKG=("curl" "gzip")
+
+    for pkg in "${INSTALL_PKG[@]}"; do
+        if ! _exists "$pkg" >/dev/null 2>&1; then
+            pkg_install "$pkg"
+        fi
+    done
+}
+
+check_srv() {
+    local -a WEB_SRV
+    WEB_SRV=("nginx" "openresty" "tengine")
+    for ((i=0; i<${#WEB_SRV[@]}; i++)); do
+        if docker ps --filter "name=${WEB_SRV[i]}" -q | grep -q .; then
+            CONTAINER_NAME="${WEB_SRV[i]}"
+            break
+        fi
+    done
+
+    if [ -z "$CONTAINER_NAME" ]; then
+        die "No matching servers found."
+    fi
+}
+
+# 日志截断
+log_rotate() {
+    local START_TIME
+    START_TIME="$(date +%Y-%m-%d)"
+
+    cd "${SCRIPT_DIR:?}" >/dev/null 2>&1 || die "Unable to enter directory."
+    mv -f logs/access.log "logs/access_$START_TIME.log" >/dev/null 2>&1
+    mv -f logs/error.log "logs/error_$START_TIME.log" >/dev/null 2>&1
+    docker exec "$CONTAINER_NAME" nginx -s reopen >/dev/null 2>&1
+    gzip "logs/access_$START_TIME.log" >/dev/null 2>&1
+    gzip "logs/error_$START_TIME.log" >/dev/null 2>&1
+    find logs -type f -name "*.log.gz" -mtime +7 -exec rm {} \; >/dev/null 2>&1
+}
+
+build_msg() {
+    END_TIME="$(date -u '+%Y-%m-%d %H:%M:%S' -d '+8 hours')"
+}
+
 send_msg() {
     local MESSAGE="$1"
 
@@ -42,10 +96,9 @@ send_msg() {
     fi
 }
 
-cd "${SCRIPT_DIR:?}" || die "Unable to enter directory."
-mv -f logs/access.log "logs/access_$START_TIME.log" >/dev/null 2>&1
-mv -f logs/error.log "logs/error_$START_TIME.log" >/dev/null 2>&1
-docker exec nginx nginx -s reopen >/dev/null 2>&1
-gzip "logs/access_$START_TIME.log" >/dev/null 2>&1
-gzip "logs/error_$START_TIME.log" >/dev/null 2>&1
-find logs -type f -name "*.log.gz" -mtime +7 -exec rm {} \; >/dev/null 2>&1
+check_root
+check_cmd
+check_srv
+log_rotate
+build_msg
+send_msg
