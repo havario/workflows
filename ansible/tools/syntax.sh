0385c9dc30f79ba4f521c19ec486fab8b30ba110
havario <157877551+havario@users.noreply.github.com>
2025-10-28 13:40:08 +0800

    update
    Signed-off-by: havario <157877551+havario@users.noreply.github.com>

---
+++ ansible/tools/syntax.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# SPDX-License-Identifier: GPL-2.0
+#
+# Copyright (c) 2021 Canonical Ltd.
+# Copyright (c) 2025 Krzysztof Kozlowski <krzk@kernel.org>
+# Copyright (c) 2025 honeok <i@honeok.com>
+
+set -eE
+
+WORK_DIR="$(dirname "${BASH_SOURCE[0]}")"
+
+for file in "$WORK_DIR"/*yml; do
+    ansible-playbook -v --syntax-check "$file"
+done
+
+exit "$?"
