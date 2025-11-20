9410f2788b5e4af9023d7aba1875aa4818f1ed5a
havario <157877551+havario@users.noreply.github.com>
2025-11-20 10:48:09 +0800

    format
    Signed-off-by: havario <157877551+havario@users.noreply.github.com>

--- webserver/openresty/lua/block.lua
+++ webserver/openresty/lua/block.lua
@@ -10,22 +10,22 @@ local request_line = ngx.var.request or ""
 
 -- 乱码请求快速关闭连接
 if string.match(request_line, "^[^A-Z]") then
-    return ngx.exit(444)
+  return ngx.exit(444)
 end
 
 -- 定义敏感文件
 local sensitive_files = {
-    "^/\\.(?!well-known)(env|git|gitignore|htaccess|hg|svn|bzr|editorconfig|npmrc|bashrc|bash_profile|bash_history|[^/]+)$",
-    "composer\\.(json|lock)$",
-    "package\\.json$",
-    "yarn\\.lock$",
-    "\\.(bak|old|swp|~$|sql|db|dump|php~|conf~|ini~|log~|pyc|pyo|sqlite)$",
-    "^/(wp-config\\.php|config\\.php|settings\\.php|database\\.yml|secrets\\.yaml)$",
-    "^/(\\.DS_Store|Thumbs\\.db|\\.idea|\\.vscode)$"
+  "^/\\.(?!well-known)(env|git|gitignore|htaccess|hg|svn|bzr|editorconfig|npmrc|bashrc|bash_profile|bash_history|[^/]+)$",
+  "composer\\.(json|lock)$",
+  "package\\.json$",
+  "yarn\\.lock$",
+  "\\.(bak|old|swp|~$|sql|db|dump|php~|conf~|ini~|log~|pyc|pyo|sqlite)$",
+  "^/(wp-config\\.php|config\\.php|settings\\.php|database\\.yml|secrets\\.yaml)$",
+  "^/(\\.DS_Store|Thumbs\\.db|\\.idea|\\.vscode)$"
 }
 
 for _, pattern in ipairs(sensitive_files) do
-    if regex_match(request_uri, pattern, "ioj") then
-        return ngx.exit(444)
-    end
+  if regex_match(request_uri, pattern, "ioj") then
+    return ngx.exit(444)
+  end
 end
