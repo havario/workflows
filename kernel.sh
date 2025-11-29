857ce516bf4945919d4e80e9671d9b76f4ecb8f6
havario <157877551+havario@users.noreply.github.com>
2025-11-29 21:00:36 +0800

    1
    Signed-off-by: havario <157877551+havario@users.noreply.github.com>

--- kernel.sh
+++ kernel.sh
@@ -22,4 +22,18 @@ linux_logo() {
 "
 }
 
+# debian/ubuntu
+# https://xanmod.org
+
+xanmod_install() {
+    local XANMOD_VER VERSION_CODE
+
+    # https://gitlab.com/xanmod/linux
+    XANMOD_VER="$(curl -L https://dl.xanmod.org/check_x86-64_psabi.sh | awk -f - 2>/dev/null | awk -F 'x86-64-v' '{v=$2+0; if(v==4)v=3; print v}')"
+    VERSION_CODE="$(grep "^VERSION_CODENAME" /etc/os-release | cut -d= -f2)"
+
+    pkg_install gnupg
+    curl -Ls https://dl.xanmod.org/archive.key | gpg --dearmor -vo /etc/apt/keyrings/xanmod-archive-keyring.gpg --yes
+}
+
 ## 主程序入口
