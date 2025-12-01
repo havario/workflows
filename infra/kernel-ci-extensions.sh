157a3765d4da797e8aa5dbc40eb48f02709455ec
havario <157877551+havario@users.noreply.github.com>
2025-12-01 17:50:51 +0800

    exec
    Signed-off-by: havario <157877551+havario@users.noreply.github.com>

--- infra/kernel-ci-extensions.sh
+++ infra/kernel-ci-extensions.sh
@@ -2,9 +2,26 @@
 # SPDX-License-Identifier: Apache-2.0
 # Copyright (c) 2025 honeok <i@honeok.com>
 
-set -x
+set -ex
 
+# 环境变量用于在debian或ubuntu操作系统中设置非交互式 (noninteractive) 安装模式
 export DEBIAN_FRONTEND=noninteractive
 
-apt-get update
-apt-get install -y bash curl
+_exists() {
+    command -v "$@" >/dev/null 2>&1
+}
+
+install_pkg() {
+    for pkg in "$@"; do
+        if _exists dnf; then
+            dnf install -y "$pkg"
+        elif _exists yum; then
+            yum install -y "$pkg"
+        elif _exists apt-get; then
+            apt-get update
+            apt-get install -y -q "$pkg"
+        fi
+    done
+}
+
+install_pkg bash curl
