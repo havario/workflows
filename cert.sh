dc8009182d8f49fea297de81ef140262f34af9c2
havario <157877551+havario@users.noreply.github.com>
2025-12-04 13:30:06 +0800

    cert
    Signed-off-by: havario <157877551+havario@users.noreply.github.com>

---
+++ cert.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# 到期时间
+openssl x509 -in /data/docker_data/nginx/certs/honeok.com_fullchain.pem -noout -dates
+echo $(( ( $(openssl x509 -in /data/docker_data/nginx/certs/honeok.com_fullchain.pem -noout -enddate | cut -d= -f2 | xargs -I{} date -d "{}" +%s) - $(date +%s) ) / 86400 ))
+
+# 证书签发者
+openssl x509 -in /data/docker_data/nginx/certs/honeok.com_fullchain.pem -noout -issuer
+
+# 证书使用域名
+openssl x509 -in /data/docker_data/nginx/certs/honeok.com_fullchain.pem -text -noout | grep -A 5 "Subject Alternative Name"
+
+# 公钥类型和长度
+openssl x509 -in /data/docker_data/nginx/certs/honeok.com_fullchain.pem -text -noout | grep "Public-Key"
+
