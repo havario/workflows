136394d9621864135874e2b907fbea2a954b7928
havario <157877551+havario@users.noreply.github.com>
2025-12-04 13:35:50 +0800

    openssl
    Signed-off-by: havario <157877551+havario@users.noreply.github.com>

--- cert.sh
+++ cert.sh
@@ -8,8 +8,7 @@ echo $(( ( $(openssl x509 -in /data/docker_data/nginx/certs/honeok.com_fullchain
 openssl x509 -in /data/docker_data/nginx/certs/honeok.com_fullchain.pem -noout -issuer
 
 # 证书使用域名
-openssl x509 -in /data/docker_data/nginx/certs/honeok.com_fullchain.pem -text -noout | grep -A 5 "Subject Alternative Name"
+openssl x509 -in /data/docker_data/nginx/certs/honeok.com_fullchain.pem -text -noout | grep -A1 "Subject Alternative Name" | tail -n1 | xargs
 
 # 公钥类型和长度
 openssl x509 -in /data/docker_data/nginx/certs/honeok.com_fullchain.pem -text -noout | grep "Public-Key"
-
