5d2fed83b8340f4cd47419268f2cdbb5090e9bd6
havario <157877551+havario@users.noreply.github.com>
2025-12-16 15:15:41 +0800

    init commit
    Signed-off-by: havario <157877551+havario@users.noreply.github.com>

---
+++ forge/dongguaTV/docker-entrypoint.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env sh
+# SPDX-License-Identifier: MIT
+# Copyright (c) 2025 honeok <i@honeok.com>
+
+set -e
+
+_info_msg() {
+    echo >&2 "$@"
+}
+
+: "${TMDB_API_KEY:?Error: TMDB_API_KEY missing.}"
+sed -i "s#const TMDB_API_KEY = \".*\";#const TMDB_API_KEY = \"$TMDB_API_KEY\";#g" public/index.html
+
+# api.themoviedb.org proxy
+if [ -n "$PROXY_URL" ]; then
+    sed -i "s#const MY_PROXY = \".*\";#const MY_PROXY = \"$PROXY_URL\";#g" public/index.html
+else
+    _info_msg "$0: No custom Proxy URL set. using default. skipping configuration"
+fi
+
+# admin password
+if [ -n "$ADMIN_PASSWD" ]; then
+    sed -i "s#const ADMIN_PASSWORD = \".*\";#const ADMIN_PASSWORD = \"$ADMIN_PASSWD\";#g" server.js
+else
+    _info_msg "$0: admin password not set, using default password 'admin'."
+fi
+
+exec "$@"
