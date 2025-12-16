4603d040a0571c8396e94b60c898fbddfcb92f7b
havario <157877551+havario@users.noreply.github.com>
2025-12-16 15:29:07 +0800

    fix
    Signed-off-by: havario <157877551+havario@users.noreply.github.com>

--- forge/dongguaTV/docker-entrypoint.sh
+++ forge/dongguaTV/docker-entrypoint.sh
@@ -5,22 +5,22 @@
 set -e
 
 _info_msg() {
-    echo >&2 "$@"
+    echo >&2 "$(date '+%Y/%m/%d %H:%M:%S') $@"
 }
 
 : "${TMDB_API_KEY:?Error: TMDB_API_KEY missing.}"
-sed -i "s#const TMDB_API_KEY = \".*\";#const TMDB_API_KEY = \"$TMDB_API_KEY\";#g" public/index.html
+sed -i "s#const TMDB_API_KEY = [\"'].*[\"'];#const TMDB_API_KEY = \"$TMDB_API_KEY\";#g" public/index.html
 
-# api.themoviedb.org proxy
+# replace api.themoviedb.org proxy
 if [ -n "$PROXY_URL" ]; then
-    sed -i "s#const MY_PROXY = \".*\";#const MY_PROXY = \"$PROXY_URL\";#g" public/index.html
+    sed -i "s#const MY_PROXY = [\"'].*[\"'];#const MY_PROXY = \"$PROXY_URL\";#g" public/index.html
 else
     _info_msg "$0: No custom Proxy URL set. using default. skipping configuration"
 fi
 
-# admin password
+# replace admin password
 if [ -n "$ADMIN_PASSWD" ]; then
-    sed -i "s#const ADMIN_PASSWORD = \".*\";#const ADMIN_PASSWORD = \"$ADMIN_PASSWD\";#g" server.js
+    sed -i "s#const ADMIN_PASSWORD = [\"'].*[\"'];#const ADMIN_PASSWORD = \"$ADMIN_PASSWD\";#g" server.js
 else
     _info_msg "$0: admin password not set, using default password 'admin'."
 fi
