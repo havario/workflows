41e44216870382a15acaf1392fed18e8fc2a11cd
havario <157877551+havario@users.noreply.github.com>
2025-12-17 10:14:35 +0800

    rm
    Signed-off-by: havario <157877551+havario@users.noreply.github.com>

--- infra/debian.sh
+++ infra/debian.sh
@@ -5,3 +5,4 @@
 echo "deb http://deb.debian.org/debian $VERSION_CODENAME-backports main" | tee /etc/apt/sources.list.d/backports.list
 apt-get update
 apt-get install -y -t "$VERSION_CODENAME-backports" "linux-image-cloud-$(dpkg --print-architecture)"
+rm -f /etc/apt/sources.list.d/backports.list
