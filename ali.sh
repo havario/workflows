452540a400f1609117948520ebf8e18fbc5718e9
havario <157877551+havario@users.noreply.github.com>
2025-12-26 03:12:06 +0800

    clear
    Signed-off-by: havario <157877551+havario@users.noreply.github.com>

--- ali.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-curl -Ls http://100.100.100.200/latest/meta-data/instance-id
-curl -Ls http://100.100.100.200/latest/meta-data/zone-id
-curl -Ls http://100.100.100.200/latest/meta-data/hostname
-
-hostnamectl set-hostname "$(curl -Ls http://100.100.100.200/latest/meta-data/hostname)"
