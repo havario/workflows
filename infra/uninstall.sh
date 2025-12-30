0cab024baa6fd3a8db444832d01a40f6fd3d0f47
havario <157877551+havario@users.noreply.github.com>
2025-12-30 10:46:44 +0800

    set
    Signed-off-by: havario <157877551+havario@users.noreply.github.com>

--- infra/uninstall.sh
+++ infra/uninstall.sh
@@ -2,7 +2,7 @@
 # SPDX-License-Identifier: Apache-2.0
 # Copyright (c) 2025 honeok <i@honeok.com>
 
-set -eEuxo pipefail
+# set -eEuxo pipefail
 
 systemctl stop docker.socket
 systemctl stop docker.service
