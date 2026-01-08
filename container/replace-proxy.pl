b591c0f9cb9090e1aa786407c676039a23a16fb2
havario <157877551+havario@users.noreply.github.com>
2026-01-08 10:52:38 +0800

    fix
    Signed-off-by: havario <157877551+havario@users.noreply.github.com>

--- container/replace-proxy.pl
+++ container/replace-proxy.pl
@@ -26,7 +26,51 @@ $^I = "";
 
 my $registry_regex = join("|", map { quotemeta($_) } @registry_repo);
 
+# 上下文捕获
 while (<>) {
-    s#(?<!\Q$registry_proxy\E/)($registry_regex)#$registry_proxy/$1#g;
+    s{
+        (^\s*image:\s*|^\s*FROM\s+(?:--platform=\S+\s+)?|--from=)
+        (["']?)
+        ([^\s"']+)
+        (["']?)
+    }{
+        $1 . $2 . process_image($3) . $4
+    }gxe;
+
     print;
+}
+
+sub process_image {
+    my ($img) = @_;
+    my $target;
+
+    # 如果已经是代理地址 静默跳过
+    return $img if $img =~ /^\Q$registry_proxy\E/;
+
+    # 显式域名检查
+    if ($img =~ m{^([^/]+)/}) {
+        my $domain = $1;
+        if ($domain =~ /\.|localhost/) {
+            if ($domain =~ /^($registry_regex)$/) {
+                $target = "$registry_proxy/$img";
+            } else {
+                return $img;
+            }
+        }
+    }
+
+    # 处理隐式 DockerHub
+    unless ($target) {
+        my $canonical = $img;
+        $canonical = "library/$img" unless $img =~ m{/};
+        $target = "$registry_proxy/docker.io/$canonical";
+    }
+
+    # 熔断机制
+    if (!defined $target || $target eq $img) {
+        die "Error: Failed to proxy image '$img'.\nReason: Logic matched modification criteria but result was identical or empty.\n";
+    }
+
+    print STDERR "\xe2\x9c\x93 $img => $target\n";
+    return $target;
 }
