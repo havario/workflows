4dacb70f6076c745caa67c4d69b6d9ddf0cf27dd
havario <157877551+havario@users.noreply.github.com>
2026-01-11 00:09:59 +0800

    go
    Signed-off-by: havario <157877551+havario@users.noreply.github.com>

--- ci/s2i.sh
+++ ci/s2i.sh
@@ -49,22 +49,53 @@ GAVE_SRC_TOP="$(realpath $1)"
 
 # Try to guest Java or nodeJs
 echo "Auto try to detect Java or Node.js source and its topdir."
+DETECT_GO="find $GAVE_SRC_TOP -maxdepth 1 -iname go.mod"
+DETECT_JAVA="find $GAVE_SRC_TOP -maxdepth 1 -iname pom.xml"
 DETECT_NODEJS="find $GAVE_SRC_TOP -maxdepth 2 -iname package.json"
 DETECT_DEFAULT_TOP="find $GAVE_SRC_TOP -maxdepth 2 -iname .TOP"
 DETECT_DEFAULT_GIT="find $GAVE_SRC_TOP -maxdepth 2 -iname .git"
+
+# Debug
+eval "$DETECT_GO"
 eval "$DETECT_JAVA"
 eval "$DETECT_NODEJS"
 
-if [ -n "$(eval "$DETECT_NODEJS")" ]; then
+if [ -n "$(eval "$DETECT_GO")" ]; then
+    GOMOD="$(echo "$(eval "$DETECT_GO")" | head -n 1)"
+    SRC_TOP="$(realpath "$(dirname "$GOMOD")")"
+    SRC_TYPE=go
+    SRC_VERSION=""
+    echo "Detect SRC_TOP from file $GOMOD"
+elif [ -n "$(eval "$DETECT_JAVA")" ]; then
+    POM="$(echo "$(eval "$DETECT_JAVA")" | head -n 1)"
+    SRC_TOP="$(echo "$(realpath "$(dirname "$POM")")" | sort | head -n 1)"
+    SRC_TYPE="java"
+    mvn --file "$SRC_TOP" -N -Dexec.executable='echo' -Dexec.args='${project.version}' org.codehaus.mojo:exec-maven-plugin:1.3.1:exec
+    SRC_VERSION="$(mvn --file "$SRC_TOP" -q -N -Dexec.executable='echo' -Dexec.args='${project.version}' org.codehaus.mojo:exec-maven-plugin:1.3.1:exec | tail -n 1)"
+    echo "Detect SRC_TOP from file $POM"
+elif [ -n "$(eval "$DETECT_NODEJS")" ]; then
     PACKAGE="$(echo "$(eval "$DETECT_NODEJS")" | head -n 1)"
     SRC_TOP="$(realpath "$(dirname "$PACKAGE")")"
-    SRC_TYPE=nodejs
+    SRC_TYPE="nodejs"
     SRC_VERSION="$(cd "$SRC_TOP" && npm run packageVersion | tail -n 1)"
-    echo "detect SRC_TOP from file $PACKAGE"
+    echo "Detect SRC_TOP from file $PACKAGE"
 fi
 
-if [ -n "$GITLAB_CI" ]; then
+if [ -z "$SRC_VERSION" ]; then
+    SRC_VERSION="1.0.0"
+fi
+SRC_GIT_COMMIT_ID="-$(cd "$SRC_TOP" && git rev-parse --short HEAD)"
+
+if [ -n "$GITHUB_ACTIONS" ]; then
+    echo "GitHub CI"
+    DOCKER_IMAGE_NAME="${GITHUB_REPOSITORY#*/}"
+    BUILD_COUNTER="-${GITHUB_REF_NAME//\//-}${SRC_GIT_COMMIT_ID}-gh-${GITHUB_RUN_NUMBER}"
+elif [ -n "$GITLAB_CI" ]; then
     echo "GITLAB CI"
     DOCKER_IMAGE_NAME="$CI_PROJECT_NAME"
-    BUILD_COUNTER="-$CI_COMMIT_REF_SLUG$SRC_GIT_COMMIT_ID-gl-$CI_BUILD_ID"
+    BUILD_COUNTER="-${CI_COMMIT_REF_SLUG}${SRC_GIT_COMMIT_ID}-gl-${CI_BUILD_ID}"
+elif [ -n "$JENKINS_URL" ]; then
+    echo "Jenkins CI"
+    DOCKER_IMAGE_NAME="$JOB_NAME"
+    BUILD_COUNTER="-jk-$BUILD_NUMBER"
 fi
